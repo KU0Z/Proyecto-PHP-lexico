641e715987ec64be3b156932a85e826d0e41a65d
Kevin Ubedo <koubedo@correo.url.edu.gt>
2018-04-04 22:33:36 -0600

    Primeras pruebas con jflex

---
+++ MiniPHP/src/miniphp/MiniPHP.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniphp;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author keviu
+ */
+public class MiniPHP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        String path =  "C:/Users/keviu/Documents/Ciclo 5/Lenguajes/Proyecto/Proyecto-PHP-lexico/MiniPHP/src/miniphp/Lexer.flex";
+        generarLexer(path);  
+        
+
+    }
+    public static void generarLexer(String path){
+        File file=new File(path);
+        jflex.Main.generate(file);
+    }
+    
+}
